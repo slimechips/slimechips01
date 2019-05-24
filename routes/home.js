95365a68a46343e273fbcb29157c8c9fa240ce74
slimechips <jason.chowcs#gmail.com>
2019-05-24 09:34:17 +0800

    change console log

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  console.log(req.body);
+  console.log("posted " + req.body);
 });
 module.exports = router;
