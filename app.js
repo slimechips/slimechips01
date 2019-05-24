6075ea17e057599b0e7fc38e459ce1266bc4bd8f
slimechips <jason.chowcs#gmail.com>
2019-05-24 09:09:11 +0800

    add home route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let redirectRouter = require('./routes/redirect');
+let homeRouter = require('./routes/home');
 
 let app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/redirect', redirectRouter);
+app.use('/home', homeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
